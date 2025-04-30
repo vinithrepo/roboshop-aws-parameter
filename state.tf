f24a6198e4ca0bc0fab36ac532382c9bf5041b63
Vinith Amma <vinithamma007@gmail.com>
2025-04-30 15:35:35 +0530

    tf practice

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3"{
     bucket         = "sbucket83"
-    key            = "parameters/state2.state"
+    key            = "parameters/s.tf-state"
     region         = "us-east-1"
   }
 }
